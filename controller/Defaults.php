85daf4a18713aa30eed8b0f78812ce1c2caa3b80
Weiping <weiping@diancloud.com>
2017-05-22 18:03:15 +0800

    MINA Pages Project Init

---
+++ controller/Defaults.php
@@ -0,0 +1,55 @@
+<?php
+use \Tuanduimao\Loader\App as App;
+use \Tuanduimao\Utils as Utils;
+use \Tuanduimao\Tuan as Tuan;
+use \Tuanduimao\Excp as Excp;
+use \Tuanduimao\Conf as Conf;
+
+
+class DefaultsController extends \Tuanduimao\Loader\Controller {
+	
+	function __construct() {
+	}
+
+	function index() {
+		$hello = App::M('Hello');
+		$data = $hello->select("LIMIT 20");
+
+		App::render($data,'web','index');
+		
+		return [
+			'js' => [
+		 			"js/plugins/select2/select2.full.min.js",
+		 			"js/plugins/jquery-validation/jquery.validate.min.js",
+		 			"js/plugins/dropzonejs/dropzone.min.js",
+		 			"js/plugins/cropper/cropper.min.js",
+		 			'js/plugins/masked-inputs/jquery.maskedinput.min.js',
+		 			'js/plugins/jquery-tags-input/jquery.tagsinput.min.js',
+			 		"js/plugins/dropzonejs/dropzone.min.js",
+			 		"js/plugins/cropper/cropper.min.js",
+		    		'js/plugins/jquery-ui/jquery-ui.min.js',
+	        		'js/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js',
+				],
+			'css'=>[
+	 			"js/plugins/select2/select2.min.css",
+	 			"js/plugins/select2/select2-bootstrap.min.css"
+	 		],
+	 		'active' =>[
+	 			'c'=>'article',
+	 			'a'=>'index'
+	 			// 'slug' => 'mina/pages/article/index'
+	 		],
+			'crumb' => [
+	            "返回图文" => APP::R('article','index'),
+	            "菜单激活演示" =>'',
+	        ]
+		];
+	}
+
+	function faker() {
+		$hello = App::M('Hello');
+		$hello->fakerdata();
+		echo json_encode($hello->select("",["COUNT(*)"]));
+	}
+
+}
