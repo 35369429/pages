b4d64facffca89aa1ad191aae0f1b9c62f5c552c
trheyi <zhuce@diancloud.com>
2017-08-14 23:54:55 +0800

    + 图说

---
+++ controller/Tushuo.php
@@ -0,0 +1,45 @@
+<?php
+use \Tuanduimao\Loader\App as App;
+use \Tuanduimao\Utils as Utils;
+use \Tuanduimao\Tuan as Tuan;
+use \Tuanduimao\Excp as Excp;
+use \Tuanduimao\Conf as Conf;
+use \Tuanduimao\Task as Task;
+use \Mina\Storage\Local as Storage;
+use \Endroid\QrCode\QrCode as Qrcode;
+use Endroid\QrCode\LabelAlignment;
+use \Endroid\QrCode\ErrorCorrectionLevel;
+
+
+
+class TushuoController extends \Tuanduimao\Loader\Controller {
+	
+	function __construct() {
+	}
+
+	function index(){
+		return [
+			'js' => [
+		 			"js/plugins/select2/select2.full.min.js",
+		 			"js/plugins/jquery-validation/jquery.validate.min.js",
+		 			"js/plugins/dropzonejs/dropzone.min.js",
+		 			"js/plugins/cropper/cropper.min.js",
+		 			'js/plugins/masked-inputs/jquery.maskedinput.min.js',
+		 			'js/plugins/jquery-tags-input/jquery.tagsinput.min.js',
+			 		"js/plugins/dropzonejs/dropzone.min.js",
+			 		"js/plugins/cropper/cropper.min.js",
+		    		'js/plugins/jquery-ui/jquery-ui.min.js',
+	        		'js/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js',
+				],
+			'css'=>[
+	 			"js/plugins/select2/select2.min.css",
+	 			"js/plugins/select2/select2-bootstrap.min.css"
+	 		],
+			'crumb' => [
+	                 "图说" => APP::R('tushuo','index'),
+	                 "图说列表" =>'',
+	        ]
+		];
+	}
+
+}
---
+++ model/Tushuo.php
@@ -0,0 +1,29 @@
+<?php
+namespace Mina\Pages\Model; 
+define('__NS__', 'Mina\Pages\Model'); // 兼容旧版 App::M 方法调用
+
+use \Tuanduimao\Loader\App as App;
+use \Tuanduimao\Mem as Mem;
+use \Tuanduimao\Excp as Excp;
+use \Tuanduimao\Err as Err;
+use \Tuanduimao\Conf as Conf;
+use \Tuanduimao\Model as Model;
+use \Tuanduimao\Utils as Utils;
+use \Tuanduimao\Wechat as Wechat;
+use \Tuanduimao\Media as Media;
+use \Mina\Delta\Render as Render;
+use \Tuanduimao\Task as Task;
+
+use \Exception as Exception;
+
+
+
+/**
+ * 图说数据模型
+ */
+class Tushuo extends Model {
+
+	function __construct( $param=[] ) {
+		
+	}
+}
