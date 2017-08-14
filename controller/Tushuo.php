7bd45189d2d80b56edb9a6659b1fa406fb465a28
trheyi <zhuce@diancloud.com>
2017-08-15 01:58:59 +0800

    + 图说  UI 框架

--- controller/Tushuo.php
+++ controller/Tushuo.php
@@ -42,4 +42,48 @@ class TushuoController extends \Tuanduimao\Loader\Controller {
 		];
 	}
 
+
+	/**
+	 * 图说编辑器
+	 * @return 
+	 */
+	function editor() {
+
+		App::render($data, 'tushuo', 'editor' );
+
+
+		return [
+
+			'js' => [
+		 			"js/plugins/select2/select2.full.min.js",
+		 			"js/plugins/jquery-tags-input/jquery.tagsinput.min.js",
+		 			"js/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js",
+		 			'js/plugins/masked-inputs/jquery.maskedinput.min.js',
+		 			"js/plugins/jquery-validation/jquery.validate.min.js",
+		    		"js/plugins/jquery-ui/jquery-ui.min.js",
+		    		"js/plugins/jquery-webeditor/webeditor.full.min.js",
+		    		"js/plugins/jquery-webeditor/panel.full.min.js"
+				],
+			'css'=>[
+				"js/plugins/bootstrap-datepicker/bootstrap-datepicker3.min.css",
+	 			"js/plugins/select2/select2.min.css",
+	 			"js/plugins/select2/select2-bootstrap.min.css",
+	 			"js/plugins/jquery-tags-input/jquery.tagsinput.min.css",
+
+	 			"js/plugins/jquery-webeditor/webeditor.full.min.css?important",
+	 			"js/plugins/jquery-webeditor/panel.full.min.css?important"
+	 		],
+
+			'crumb' => [
+	                "图说" => APP::R('tushuo','index'),
+	                "图说列表" => APP::R('tushuo','index'),
+	                "编辑图说" => '',
+	        ],
+
+	        'active'=> [
+	 			'slug'=>'mina/pages/tushuo/index'
+	 		]
+		];
+	}
+
 }
