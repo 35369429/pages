dde5f30e1b32a50bfa7634e3c563ee8ecd3eb97f
trheyi <zhuce@diancloud.com>
2017-08-23 23:04:36 +0800

    更换LOGO + 图说背景

--- controller/Tushuo.php
+++ controller/Tushuo.php
@@ -36,8 +36,8 @@ class TushuoController extends \Tuanduimao\Loader\Controller {
 	 			"js/plugins/select2/select2-bootstrap.min.css"
 	 		],
 			'crumb' => [
-	                 "图说" => APP::R('tushuo','index'),
-	                 "图说列表" =>'',
+	                "图说" => APP::R('tushuo','index'),
+	                "图说列表" =>'',
 	        ]
 		];
 	}
@@ -58,10 +58,13 @@ class TushuoController extends \Tuanduimao\Loader\Controller {
 		 			"js/plugins/select2/select2.full.min.js",
 		 			"js/plugins/jquery-tags-input/jquery.tagsinput.min.js",
 		 			"js/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js",
-		 			'js/plugins/masked-inputs/jquery.maskedinput.min.js',
+		 			"js/plugins/masked-inputs/jquery.maskedinput.min.js",
+		 			"js/plugins/bootstrap-colorpicker/bootstrap-colorpicker.min.js",
+
 		 			"js/plugins/jquery-validation/jquery.validate.min.js",
 		    		"js/plugins/jquery-ui/jquery-ui.min.js",
 		    		"js/plugins/jquery-webeditor/webeditor.full.min.js",
+		    		"js/plugins/jquery-imgeditor/imgeditor.full.min.js",
 		    		"js/plugins/jquery-webeditor/panel.full.min.js"
 				],
 			'css'=>[
@@ -69,8 +72,10 @@ class TushuoController extends \Tuanduimao\Loader\Controller {
 	 			"js/plugins/select2/select2.min.css",
 	 			"js/plugins/select2/select2-bootstrap.min.css",
 	 			"js/plugins/jquery-tags-input/jquery.tagsinput.min.css",
+	 			"js/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css",
 
 	 			"js/plugins/jquery-webeditor/webeditor.full.min.css?important",
+	 			"js/plugins/jquery-imgeditor/imgeditor.full.min.css?important",
 	 			"js/plugins/jquery-webeditor/panel.full.min.css?important"
 	 		],
 
