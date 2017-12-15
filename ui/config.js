3bf74c1b50bf4b03d3bf08c115f672a11c8a43cd
trheyi <zhuce@diancloud.com>
2017-12-15 17:11:52 +0800

    + 默认模板

---
+++ ui/config.js
@@ -0,0 +1,13 @@
+config = {
+
+	mina:{
+		target: false,
+		server:"http://wss.xpmjs.com/",
+		domain:"wss.xpmjs.com",
+		project:"pages",
+		appid:'O4ZMbZHMa5jF8LVr',
+		secret:'mEfQ9BukRqmfY3JdbpTWv48QNr0Y3BWF'
+	}
+};
+
+module.exports = config
