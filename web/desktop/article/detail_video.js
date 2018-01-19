66eed1605071d8450fac0a7167e4971b9a390f4c
gaomaoshun <449812984@qq.com>
2018-01-19 14:08:55 +0800

    提交
    提交

---
+++ web/desktop/article/detail_video.js
@@ -0,0 +1,14 @@
+
+let web = getWeb();
+Page({
+	data:{},
+	
+	onReady: function( get ) {
+		console.log( 'page onReady data=', data  );
+	},
+
+	hello: function ( event ) {
+	},
+
+	world: null
+})
---
+++ web/web.js
@@ -0,0 +1,7 @@
+Web({
+	title:'hello',
+	onError:function( error ) {
+		
+		console.log( 'Error=', error, SERVICE_URL );
+	}
+});
