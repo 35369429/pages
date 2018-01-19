f01a995215745c9b4160941e4cb3216928a9eb08
trheyi <zhuce@diancloud.com>
2018-01-19 14:12:47 +0800

    update

--- api/Article.php
+++ api/Article.php
@@ -175,10 +175,10 @@ class Article extends Api {
 		$art = new \Xpmsns\pages\Model\Article;
 		$qb = $art->query()
 				  ->join("article_category as ac", 'ac.article_id', '=', 'article.article_id')
-				  ->leftJoin('category as c', "c.category_id", '=', 'ac.category_id')
-				  ->leftJoin("article_tag as at", 'at.article_id', '=', 'article.article_id')
-				  ->leftJoin("article_draft as draft", 'draft.article_id', '=', 'article.article_id')
-				  ->leftJoin("tag as t", 't.tag_id', '=', 'at.tag_id');
+				  ->rightJoin('category as c', "c.category_id", '=', 'ac.category_id')
+				  ->rightJoin("article_tag as at", 'at.article_id', '=', 'article.article_id')
+				  ->rightJoin("article_draft as draft", 'draft.article_id', '=', 'article.article_id')
+				  ->rightJoin("tag as t", 't.tag_id', '=', 'at.tag_id');
 
 		// 设定查询条件
 		$this->qb( $qb, 'c.name', 'category', $query, ["and", "or", "in"] );
