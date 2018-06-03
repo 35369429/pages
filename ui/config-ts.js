9aa0ac94363b57f42a11f83ce9d0dbaefd1a805b
trheyi <zhuce@diancloud.com>
2018-06-03 21:48:04 +0800

    + 涛声网

---
+++ ui/config-ts.js
@@ -0,0 +1,17 @@
+config = {
+	mina:{
+		target: false,
+		priority:0,
+		server:"http://www.hcit360.com",
+		domain:"www.hcit360.com",
+		project:"default",
+		appid:'152744030091631',
+		secret:'56f07a1328d4d8eeeaae3b76b05b7384'
+	},
+	wxapp:{
+		"cli":"/Applications/wechatwebdevtools.app/Contents/Resources/app.nw/bin/cli",
+		"appid": "wx8f62a28a0ddd0949",
+		"secret": "c29abedee3ba815d4322bbca8ba8032b"
+	}
+};
+module.exports = config
