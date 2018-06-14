1dd2ecb65a5111a6eacbd1c6d288de6bc9ddeb8a
高茂顺 <449812984@qq.com>
2018-06-14 10:28:22 +0800

    关键词
    关键词

--- api/Recommend.php
+++ api/Recommend.php
@@ -34,6 +34,11 @@ class Recommend extends Api {
 
 		$inst = new \Xpmsns\Pages\Model\Recommend;
 		$keywords = !empty($data['keywords']) ?  explode(',',$data['keywords']) : []; 
+
+		echo "<pre>";
+		var_dump($keywords);
+		echo "</pre>";
+
 		$page = !empty($data['page']) ?  $data['page'] : 1; 
 		$perpage = !empty($data['perpage']) ?  $data['perpage'] : 20; 
       	$now = !empty($data['now']) ?  $data['now'] : null; 
@@ -43,7 +48,9 @@ class Recommend extends Api {
 			return $inst->getArticlesBySlug( $data['slug'], $keywords, $now, $page, $perpage);
 		} else if ( array_key_exists('recommend_id', $data) && !empty($data['recommend_id']) ) {
 			return $inst->getArticles( $data['recommend_id'], $keywords, $now, $page, $perpage);
-		}
+		}	
+		echo "<pre>";
+		var_dump()
 
 		throw new Excp('错误的查询参数', 402, ['query'=>$query, 'data'=>$data]);
 	}
