75597914703616329e02b68042753c5d43e57f72
Max <zhuce@diancloud.com>
2019-03-02 23:02:53 +0800

    + 活动报名

---
+++ api/Userevent.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Class Userevent 
+ * 报名数据接口 
+ *
+ * 程序作者: XpmSE机器人
+ * 最后修改: 2019-03-02 22:59:58
+ * 程序母版: /data/stor/private/templates/xpmsns/model/code/api/Name.php
+ */
+namespace Xpmsns\Pages\Api;
+            
+
+use \Xpmse\Loader\App;
+use \Xpmse\Excp;
+use \Xpmse\Utils;
+use \Xpmse\Api;
+
+class Userevent extends Api {
+
+	/**
+	 * 报名数据接口
+	 */
+	function __construct() {
+		parent::__construct();
+	}
+
+	/**
+	 * 自定义函数 
+	 */
+
+
+
+
+
+
+
+
+
+
+
+}
